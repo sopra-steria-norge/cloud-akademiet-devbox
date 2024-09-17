818eda4b2fc4b44a7b338839d1dd388baeed4b4e
Mats Estensen <mats.estensen@soprasteria.com>
2024-09-17 12:29:56 +0200

    fix: clarify script

--- customizations/winget.ps1
+++ customizations/winget.ps1
@@ -4,15 +4,16 @@ winget install "Microsoft.SQLServerManagementStudio"
 winget install "Microsoft.SQLServer.2022.Express"
 winget install "Microsoft.DotNet.Framework.DeveloperPack_4"
 
-## REQUIRED
-## The following steps need to be done after initial Dev Box login
-# Update WSL
-wsl --update
-# Restart computer after this step
 
-# Install Docker Desktop
+## REQUIRED TO GET DOCKER UP AND RUNNING
+## The following steps need to be done after initial Dev Box login
+# 1. Update WSL
+wsl --update
+# 2. Restart computer after this step
+# 3. Install Docker Desktop
 winget install --id=Docker.DockerDesktop -e
-# Restart computer again after this step
+# 4. Restart computer again after this step
+
 
 # Optional: Update existing software if needed
 winget upgrade --id Microsoft.WindowsTerminal
