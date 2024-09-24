4e23abdf8eb4ef03945e0c09cdf797eb9827dea3
Mats Estensen <matsest@mxe.no>
2024-09-24 10:29:16 +0200

    fix: logic

--- scripts/hibernate-devboxes.ps1
+++ scripts/hibernate-devboxes.ps1
@@ -16,9 +16,14 @@ $devboxes = Get-AzDevCenterUserDevBox -Endpoint $project.DevCenterUri
 Write-Host "- Found $($devboxes.Count) devboxes in total"
 
 # Hibernate all running devboxes
-$runningDevboxes = $devboxes | Where-Object PowerState -ne "Hibernated"
+$runningDevboxes = $devboxes | Where-Object { $_.PowerState -ne "Hibernated" }
+if(!$runningDevboxes)[
+  Write-Host "- No running devboxes found. Exiting..."
+  exit 0
+}
+
 Write-Host "- Hibernating all running devboxes ($($runningDevboxes.Count)):"
-$devboxes | Where-Object PowerState -ne "Hibernated" | ForEach-Object -Parallel {
+$runningDevboxes | ForEach-Object -Parallel {
   Write-Host "  - Hibernating [$($_.Name)]..."
   $null = Stop-AzDevCenterUserDevBox -Endpoint $using:project.DevCenterUri -ProjectName $using:project.Name -UserId $_.User -Name $_.Name -Hibernate
 }
