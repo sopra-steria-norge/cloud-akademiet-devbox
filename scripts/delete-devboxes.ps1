d850d275626faccc2600811294e1183d3c3287fc
Mats Estensen <mats.estensen@soprasteria.com>
2024-10-23 23:57:42 +0200

    feat: add deletion script

---
+++ scripts/delete-devboxes.ps1
@@ -0,0 +1,28 @@
+[CmdletBinding(SupportsShouldProcess)]
+param (
+  [Parameter()]
+  [string]
+  $ResourceGroupName = "cloudakademiet24-rg"
+)
+
+$ErrorActionPreference = "Stop"
+
+# Find project
+$project = Get-AzDevCenterAdminProject -ResourceGroupName $ResourceGroupName
+Write-Host "> Working in DevCenter Project: $($project.Name)"
+
+# Find all devboxes
+$devboxes = Get-AzDevCenterUserDevBox -Endpoint $project.DevCenterUri
+Write-Host "- Found $($devboxes.Count) devboxes in total"
+
+if (!$devboxes) {
+  Write-Host "- No devboxes found. Exiting..."
+  exit 0
+}
+
+Write-Host "- Deleting all running devboxes ($($devboxes.Count)):"
+$runningDevboxes | ForEach-Object -Parallel {
+  $WhatIfPreference = $using:WhatIfPreference
+  Write-Host "  - Deleting [$($_.Name)]..."
+  $null = Remove-AzDevCenterUserDevBox -Endpoint $using:project.DevCenterUri -ProjectName $using:project.Name -UserId $_.User -Name $_.Name -WhatIf:$WhatIfPreference
+}
